c1f0672fd65ad4704b3a3a2167e2597612a1e45d
Nephi Draper <nephidraper@gmail.com>
2020-11-17 05:53:14 -0700

    New javascript week 1

---
+++ lesson.js
@@ -0,0 +1,8 @@
+function getName(){
+ prompt("Please enter your name.");
+}
+var name= prompt();
+function response(){
+  alert("Hello "+ name + ", Thank you for reviwing my resume site.")
+}
+response();
