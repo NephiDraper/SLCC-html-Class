ca795451bbcd14a346fa744710d189e72b62483e
Nephi Draper <nephidraper@gmail.com>
2020-11-17 06:03:30 -0700

    Fixed Javascript week 1

--- lesson.js
+++ lesson.js
@@ -1,7 +1,4 @@
-function getName(){
- prompt("Please enter your name.");
-}
-var name= prompt();
+var name= prompt("Please enter your name.");
 function response(){
   alert("Hello "+ name + ", Thank you for reviwing my resume site.")
 }
