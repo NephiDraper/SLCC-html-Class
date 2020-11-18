214d11d85fd37dd3624758cfaf46c08e7db07cdf
Nephi Draper <nephidraper@gmail.com>
2020-11-17 21:31:29 -0700

    Conditional Statment Week 2

--- lesson.js
+++ lesson.js
@@ -1,5 +1,18 @@
 var name= prompt("Please enter your name.");
-function response(){
-  alert("Hello "+ name + ", Thank you for reviwing my resume site.")
-}
+   var d = new Date();
+   var hour =d.getHours();
+   function response(){
+   
+   if(hour >= 5 && hour <=12 ){
+       alert("Good Morning "+ name + ", Thank you for reviwing my resume site.")
+   }
+   else if (hour >=12 && hour <=18) {
+       alert("Good Afternoon "+ name + ", Thank you for reviwing my resume site.")
+   }
+   
+   else {
+      alert("Good evening "+ name + ", Thank you for reviwing my resume site.")
+   }
+}//end of function 
+
 response();
