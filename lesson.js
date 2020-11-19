8f24623d268ee517567a601d5b5fe479e46be8a7
Nephi Draper <nephidraper@gmail.com>
2020-11-18 20:15:35 -0700

    test

--- lesson.js
+++ lesson.js
@@ -1,9 +1,9 @@
-var name= prompt("Please enter your name.");
+var name= prompt("Welcome to my site. Please enter your name.");
    var d = new Date();
    var hour =d.getHours();
    function response(){
    
-   if(hour >= 5 && hour <=12 ){
+   if(hour >= 5 && hour <=11 ){
        alert("Good Morning "+ name + ", Thank you for reviwing my resume site.")
    }
    else if (hour >=12 && hour <=18) {
@@ -13,6 +13,7 @@ var name= prompt("Please enter your name.");
    else {
       alert("Good evening "+ name + ", Thank you for reviwing my resume site.")
    }
-}//end of function 
+}//End of function 
 
 response();
+// 
